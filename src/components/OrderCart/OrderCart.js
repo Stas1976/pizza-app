95000df0629b272bde8fa9d035400d7f4e7ba011
Stanislavas Goldyckis <stanislavas.goldyckis@yahoo.com>
2019-08-26 17:55:47 +0300

    added souse

--- src/components/OrderCart/OrderCart.js
+++ src/components/OrderCart/OrderCart.js
@@ -43,7 +43,6 @@ const OrderCart = ({
             <i className="fas fa-trash-alt"></i>
           </div>
         </div>
-        <hr className={style.hr} />
       </li>
     );
   });
--- src/components/Padazas/Padazas.js
+++ src/components/Padazas/Padazas.js
@@ -1,15 +1,40 @@
 import React from 'react';
 import style from './index.module.scss';
 import padazas from '../../assets/images/padazas.jpg';
+import { connect } from 'react-redux';
+import * as action from '../../actions/cartAction';
+import { db } from '../../assets/db/db';
 
-const Padazas = () => {
-  return (
-    <div className={style.container}>
-      <span className={style.spn}>Padažas</span>
-      <button>Į krepšelį</button>
-      <img src={padazas} alt="" />
-    </div>
-  );
+class Padazas extends React.Component {
+  render() {
+    const { addItemToCart } = this.props;
+    const padazas = db.Padazas.map(item => {
+      return (
+        <li key={item.id}>
+          {item.name}
+          {item.about}
+          {item.price}
+          <button onClick={() => addItemToCart(item)}>Į krepšelį</button>
+        </li>
+      );
+    });
+
+    return (
+      <div className={style.container}>
+        <span className={style.spn}>Padažas</span>
+        <ul>{padazas}</ul>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    ketcupQuantity: state.cartReducer
+  };
 };
 
-export default Padazas;
+export default connect(
+  mapStateToProps,
+  action
+)(Padazas);
