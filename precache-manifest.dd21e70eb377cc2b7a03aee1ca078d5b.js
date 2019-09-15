fc956b0c8f6cd9a8b5a8a8b219c6cacea29b875c
Stanislavas Goldyckis <stanislavas.goldyckis@yahoo.com>
2019-09-15 19:43:52 +0300

    Updates

---
+++ precache-manifest.dd21e70eb377cc2b7a03aee1ca078d5b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7925647d8e274ca7f3917eb0a90bbffc",
+    "url": "/pizza-app/index.html"
+  },
+  {
+    "revision": "50289c8c9dc130da9329",
+    "url": "/pizza-app/static/css/main.dcf65fb4.chunk.css"
+  },
+  {
+    "revision": "6ad8c67304595892618c",
+    "url": "/pizza-app/static/js/2.d3336375.chunk.js"
+  },
+  {
+    "revision": "50289c8c9dc130da9329",
+    "url": "/pizza-app/static/js/main.e448e7c1.chunk.js"
+  },
+  {
+    "revision": "17733d337c9f1bf0a613",
+    "url": "/pizza-app/static/js/runtime~main.71e678ff.js"
+  },
+  {
+    "revision": "15cf82a8b5b2c81dee925c63846551e5",
+    "url": "/pizza-app/static/media/brusketa.15cf82a8.jpg"
+  },
+  {
+    "revision": "66e56505ddb34377eb9204c5f802a473",
+    "url": "/pizza-app/static/media/cezario.66e56505.jpg"
+  },
+  {
+    "revision": "2889f5f4137ef53c8758f0c7c9122c06",
+    "url": "/pizza-app/static/media/chickenBBQ.2889f5f4.jpg"
+  },
+  {
+    "revision": "11d8e7b9af01be602126c951fd76f46e",
+    "url": "/pizza-app/static/media/crazy.11d8e7b9.jpg"
+  },
+  {
+    "revision": "6af393d1ab558eb9cebc948dc14debb3",
+    "url": "/pizza-app/static/media/dodo.6af393d1.jpg"
+  },
+  {
+    "revision": "9013caf6068b9fab03d8a9c8c3ef8d56",
+    "url": "/pizza-app/static/media/graikiskas.9013caf6.jpg"
+  },
+  {
+    "revision": "817f1507804dac59da26a12fb889cc64",
+    "url": "/pizza-app/static/media/margherita.817f1507.jpg"
+  },
+  {
+    "revision": "acf26594c640bf085eae47048dabad46",
+    "url": "/pizza-app/static/media/mexican.acf26594.jpg"
+  },
+  {
+    "revision": "a7ef09a1311e4315c9123331b8d4f56d",
+    "url": "/pizza-app/static/media/mufinas.a7ef09a1.jpg"
+  },
+  {
+    "revision": "ea788dd3b9ab14f17056dcbf45144f7f",
+    "url": "/pizza-app/static/media/pizza.ea788dd3.png"
+  },
+  {
+    "revision": "fefdb53b55d4b8505a2f512ed344fa95",
+    "url": "/pizza-app/static/media/royal.fefdb53b.jpg"
+  },
+  {
+    "revision": "ec6f54429d670c74a347f9ad4dab3220",
+    "url": "/pizza-app/static/media/seafood.ec6f5442.jpg"
+  },
+  {
+    "revision": "7c918ca26ec1e08adad8563051416914",
+    "url": "/pizza-app/static/media/seasons4.7c918ca2.jpg"
+  },
+  {
+    "revision": "334f00576aa6c0f47d873976e5cebaa2",
+    "url": "/pizza-app/static/media/sparneliai.334f0057.jpg"
+  }
+]);
